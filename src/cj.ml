2ec1b7abe51b3058e5bb0e5d5e82d4f0eb17c97e
Dmitry Ponyatov <dponyatov@gmail.com>
2024-10-03 11:01:44 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/cj.ml
+++ src/cj.ml
@@ -1,3 +1,14 @@
 open Batteries
 
-let () = print_endline (dump Sys.argv)
+let read file:string =
+  let ic = open_in file in
+  let data = try input_lines ic with End_of_file -> "" in 
+  close_in ic;
+  data
+
+
+  "lib/cj.cj" |> open_in |> input_line
+
+let () =
+  print_endline (dump Sys.argv);
+  print_endline (Sys.argv |> (fun skip arr -> Array.tail arr skip) 1 |> dump)
