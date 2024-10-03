f0129dfb11495d336ac1cc3e245dc0fe0a5c3403
Dmitry Ponyatov <dponyatov@gmail.com>
2024-10-04 01:19:00 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ vscode/src/extension.ml
@@ -0,0 +1,19 @@
+(* // *)
+
+(* open Js_of_ocaml *)
+
+(* let () = 
+  Js.import("vscode"); *)
+
+(* let () =  *)
+print_endline "Hello"
+
+(* (* open Js_of_ocaml.Js
+
+let () = Js.export "vscode"  *)
+
+open! Js_of_ocaml
+
+let hello = print_endline "HEllo, cj"
+
+let sayHello x = print_endline x *)
