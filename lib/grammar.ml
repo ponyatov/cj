2d8d2a98c710290e97e108c6342795a1c9ed7928
Dmitry Ponyatov <dponyatov@gmail.com>
2024-10-05 21:13:33 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/grammar.ml
+++ lib/grammar.ml
@@ -11,23 +11,24 @@ type expr =
   | Infix of op * expr * expr
 
 (** pretty-print *)
-let rec pp (some : expr) : string =
+let rec dump (some : expr) : string =
   match some with
-  | Int n -> string_of_int n
-  | Id s -> s
-  | Pfx (Add, e) -> "+" ^ pp e
-  | Pfx (Sub, e) -> "-" ^ pp e
+  | Int n -> "int:" ^ string_of_int n
+  | Id s -> "id:" ^ s
+  | Pfx (Add, e) -> "+ " ^ dump e
+  | Pfx (Sub, e) -> "- " ^ dump e
   | Pfx (Mul, _e) -> failwith " * expr "
   | Pfx (Div, _e) -> failwith " / expr "
-  | Infix (Add, e1, e2) -> pp e1 ^ "+" ^ pp e2
-  | Infix (Sub, e1, e2) -> pp e1 ^ "-" ^ pp e2
-  | Infix (Mul, e1, e2) -> pp e1 ^ "*" ^ pp e2
-  | Infix (Div, e1, e2) -> pp e1 ^ "/" ^ pp e2
+  | Infix (Add, e1, e2) -> dump e1 ^ " + " ^ dump e2
+  | Infix (Sub, e1, e2) -> dump e1 ^ " - " ^ dump e2
+  | Infix (Mul, e1, e2) -> dump e1 ^ " * " ^ dump e2
+  | Infix (Div, e1, e2) -> dump e1 ^ " / " ^ dump e2
 
-let%test "int" = "1" = (Int 1 |> pp)
-let%test "id" = "x" = (Id "x" |> pp)
-let%test "+pfx" = "+2" = (Pfx (Add, Int 2) |> pp)
-let%test "-pfx" = "-3" = (Pfx (Sub, Int 3) |> pp)
+let%test "int" = "1" = (Int 1 |> dump)
+let%test "id" = "x" = (Id "x" |> dump)
+let%test "+pfx" = "+2" = (Pfx (Add, Int 2) |> dump)
+let%test "-pfx" = "-3" = (Pfx (Sub, Int 3) |> dump)
+let pp (_fmt : Format.formatter) (t : expr) : unit = t |> dump |> print_endline
 
 (* let () = print_endline (Cjl.pp (Pfx (Add, Int 1))); print_endline (pp (Pfx
    (Sub, Int 2))); (* print_endline (pp (Pfx (Mul, Int 3))); *) (* print_endline
